05a157a2f3adcce6332f0e618511bb881b5a88e6
Ericucho31 <101685717+Ericucho31@users.noreply.github.com>
2025-03-15 18:33:10 -0700

    UPDATE: Creacion de Ruteo y paginacion basica. PokemonCard

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/presentation/screens/all_pokemon_screen.dart';
+import 'package:flutter_pokedex/presentation/widgets/pokemon_card.dart';
 
 // Literalmente ejecuta la aplicacion
 void main() {
@@ -16,7 +18,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  DefaultTabController(length: 2,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Flutter Pokedex"),
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              bottom: const TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.directions_car)),
+                  Tab(icon: Icon(Icons.directions_transit)),
+                ],
+              ),
+            ),
+            body:  const TabBarView(
+                children: [
+                  MyHomePage(title: "HomePage"),
+                  AllPokemonScreen()
+                ]
+            ),
+          )),
     );
   }
 }
@@ -54,29 +74,16 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Mi primera aplicacion con Flutter:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+        child: Wrap(
+          children: [
+            PokemonCard(name: "Ditto", url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/132.png"),
+            PokemonCard(name: "Ditto", url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/132.png"),
+            PokemonCard(name: "Ditto", url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/132.png"),
+            PokemonCard(name: "Ditto", url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/132.png"),
+            PokemonCard(name: "Ditto", url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/132.png"),
+            PokemonCard(name: "pepe", url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/132.png")
           ],
-        ),
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
